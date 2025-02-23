749158acac011259d7d13297bea3b979c73c8ea0
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-23 15:49:10 +0100

    correcting the classname

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function AccordionItem({ num, title, text }) {
   return (
     <div className="item">
       <p className="number">{num}</p>
-      <p className="text">{title}</p>
+      <p className="title">{title}</p>
       <p className="icon">-</p>
       <div className="content-box">{text}</div>
     </div>
