26026c5276987faa81f07613be64e953ac70f49a
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-23 15:51:26 +0100

    conditionally rendering the number

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function Accordion({ data }) {
 function AccordionItem({ num, title, text }) {
   return (
     <div className="item">
-      <p className="number">{num}</p>
+      <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">-</p>
       <div className="content-box">{text}</div>
